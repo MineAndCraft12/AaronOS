b264cf398a6e3f5d5762928c7d8b1ae0b6ba7320
Aaron Adams <aadams3@pvlearners.net>
2019-04-18 00:21:19 +0000

    Hidden iFrame Browser app

---
+++ ifbHomepage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>iFrame Browser Homepage</title>
+    </head>
+    <body>
+        <h1>iFrame Browser</h1>
+        <p>This app is meant for testing purposes.</p>
+        <p>Use the URL bar above and press "Go" to visit the URL.</p>
+        <p>If AaronOS is loaded over HTTPS, you won't be able to visit HTTP sites.</p>
+        <p>Many websites block iFrames as well.</p>
+    </body>
+</html>
