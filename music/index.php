1fe798373628ac7fd2fd5fe95ffbbf01adf6ad49
Aaron Adams <aadams3@pvlearners.net>
2019-05-12 00:21:08 +0000

    New visualizers and color schemes in MPl

--- music/index.php
+++ music/index.php
@@ -9,7 +9,11 @@
     <body style="margin:0;padding:0;width:100%;height:100%;overflow:hidden;">
         <div class="winHTML" style="width:calc(100% - 8px);height:calc(100% - 6px);margin:0;padding:3px;padding-right:5px;overflow-y:auto;overflow-x:hidden">
             <div id="introduction">Choose a FOLDER to use in aOS Music Player. These files will NOT be uploaded to the AaronOS server.<br>
-            <input type="file" id="folderInput" webkitdirectory directory multiple onchange="loadFolder()"><br><br></div>
+            <input type="file" id="folderInput" webkitdirectory directory multiple onchange="loadFolder()"><br><br>
+            Or, if you want to select individual files, do that here.<br>
+            <input type="file" id="fileInput" multiple accept="audio/*" onchange="loadFiles()"><br><br>
+            If you have a weird filetype or I'm not detecting your files, try and force play it here:<br>
+            <input type="file" id="fileWeirdInput" multiple onchange="loadWeirdFiles()"></div>
             <div id="currentlyPlaying" class="disabled">No Song Playing</div>
             <div id="controls" class="disabled">
                 <button onclick="play()">Play</button>
@@ -20,12 +24,16 @@
                 <button onclick="refresh()">Refresh</button>
                 <br>
                 Visualizer: <select id="visfield" onchange="setVis(this.value)"></select> |
-                Color Scheme: <select id="colorfield" onchange="setColor(this.value)"></select>
+                Color Scheme: <select id="colorfield" onchange="setColor(this.value)"></select> |
+                Delay: <input style="width: 50px" type="number" id="delayinput" min="0" max="1" value="0.17" step="0.01" onchange="setDelay(this.value)"> |
+                <button onclick="toggleFPS()">FPS</button>
             </div>
             <div id="progressContainer" class="disabled"><div id="progress"></div></div>
             <div id="songList" class="disabled"></div>
             <div id="visualizer" class="disabled"><canvas id="visCanvas" onclick="toggleFullscreen()"></canvas></div>
         </div>
     </body>
-    <script defer src="script.js"></script>
+    <?php
+        echo '<script defer src="script.js?ms='.round(microtime(true) * 1000).'"></script>'
+    ?>
 </html>
--- music/script.js
+++ music/script.js
@@ -1,3 +1,7 @@
+window.onerror = function(errorMsg, url, lineNumber){
+    alert("oof, u got a error\n\n" + url + '[' + lineNumber + ']:\n' + errorMsg)
+}
+
 function getId(target){
     return document.getElementById(target);
 }
@@ -17,12 +21,17 @@ var audioContext;
 var mediaSource;
 
 var delayNode;
+function setDelay(newDelay){
+    delayNode.delayTime.value = (newDelay || 0);
+}
 
 var analyser;
 
 var visData;
 
 var folderInput = getId("folderInput");
+var fileInput = getId("fileInput");
+var fileWeirdInput = getId("fileWeirdInput");
 var songList = getId("songList");
 var progressBar = getId("progress");
 var currentlyPlaying = getId("currentlyPlaying");
@@ -32,7 +41,7 @@ var filesAmount = 0;
 var fileNames = [];
 var filesLength = 0;
 
-var supportedFormats = ['wav', 'mp3', 'ogg', 'flac'];
+var supportedFormats = ['aac', 'aiff', 'wav', 'm4a', 'mp3', 'amr', 'au', 'weba', 'oga', 'wma', 'flac', 'ogg', 'opus', 'webm'];
 
 var URL;
 
@@ -53,8 +62,11 @@ function loadFolder(event){
     fileNames = [];
     for(var i = 0; i < filesAmount; i++){
         var fileName = files[i].name.split('.');
-        if(supportedFormats.indexOf(fileName.pop()) !== -1){
+        if(files[i].type.indexOf("audio/") === 0){
             filesLength++;
+            if(supportedFormats.indexOf(fileName[fileName.length - 1]) > -1){
+                fileName.pop();
+            }
             fileNames.push([fileName.join('.'), i, URL.createObjectURL(files[i])]);
         }
     }
@@ -83,6 +95,120 @@ function loadFolder(event){
     getId("introduction").classList.add('disabled');
     getId("visualizer").classList.add('disabled');
     setVis("none");
+    
+    winsize = [window.innerWidth, window.innerHeight];
+    if(fullscreen){
+        size = [window.innerWidth, window.innerHeight];
+    }else{
+        size = [window.innerWidth - 8, window.innerHeight - 81];
+    }
+    getId("visCanvas").width = size[0];
+    getId("visCanvas").height = size[1];
+    
+    requestAnimationFrame(globalFrame);
+}
+
+function loadFiles(event){
+    audio.pause();
+    currentSong = -1;
+    files = fileInput.files;
+    filesAmount = files.length;
+    filesLength = 0;
+    fileNames = [];
+    for(var i = 0; i < filesAmount; i++){
+        var fileName = files[i].name.split('.');
+        if(files[i].type.indexOf("audio/") === 0){
+            filesLength++;
+            if(supportedFormats.indexOf(fileName[fileName.length - 1]) > -1){
+                fileName.pop();
+            }
+            fileNames.push([fileName.join('.'), i, URL.createObjectURL(files[i])]);
+        }
+    }
+    listSongs();
+    var disabledElements = document.getElementsByClassName('disabled');
+    while(disabledElements.length > 0){
+        disabledElements[0].classList.remove('disabled');
+    }
+    
+    audioContext = new AudioContext();
+    mediaSource = audioContext.createMediaElementSource(audio);
+    
+    delayNode = audioContext.createDelay();
+    delayNode.delayTime.value = 0.17;
+    delayNode.connect(audioContext.destination);
+    
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 32768;
+    analyser.minDecibels = -70;
+    analyser.smoothingTimeConstant = 0;
+    mediaSource.connect(analyser);
+    analyser.connect(delayNode);
+    
+    visData = new Uint8Array(analyser.frequencyBinCount);
+    
+    getId("introduction").classList.add('disabled');
+    getId("visualizer").classList.add('disabled');
+    setVis("none");
+    
+    winsize = [window.innerWidth, window.innerHeight];
+    if(fullscreen){
+        size = [window.innerWidth, window.innerHeight];
+    }else{
+        size = [window.innerWidth - 8, window.innerHeight - 81];
+    }
+    getId("visCanvas").width = size[0];
+    getId("visCanvas").height = size[1];
+    
+    requestAnimationFrame(globalFrame);
+}
+
+function loadWeirdFiles(event){
+    audio.pause();
+    currentSong = -1;
+    files = fileWeirdInput.files;
+    filesAmount = files.length;
+    filesLength = 0;
+    fileNames = [];
+    for(var i = 0; i < filesAmount; i++){
+        var fileName = files[i].name;
+        fileNames.push([fileName, i, URL.createObjectURL(files[i])]);
+    }
+    listSongs();
+    var disabledElements = document.getElementsByClassName('disabled');
+    while(disabledElements.length > 0){
+        disabledElements[0].classList.remove('disabled');
+    }
+    
+    audioContext = new AudioContext();
+    mediaSource = audioContext.createMediaElementSource(audio);
+    
+    delayNode = audioContext.createDelay();
+    delayNode.delayTime.value = 0.17;
+    delayNode.connect(audioContext.destination);
+    
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 32768;
+    analyser.minDecibels = -70;
+    analyser.smoothingTimeConstant = 0;
+    mediaSource.connect(analyser);
+    analyser.connect(delayNode);
+    
+    visData = new Uint8Array(analyser.frequencyBinCount);
+    
+    getId("introduction").classList.add('disabled');
+    getId("visualizer").classList.add('disabled');
+    setVis("none");
+    
+    winsize = [window.innerWidth, window.innerHeight];
+    if(fullscreen){
+        size = [window.innerWidth, window.innerHeight];
+    }else{
+        size = [window.innerWidth - 8, window.innerHeight - 81];
+    }
+    getId("visCanvas").width = size[0];
+    getId("visCanvas").height = size[1];
+    
     requestAnimationFrame(globalFrame);
 }
 
@@ -206,11 +332,20 @@ function toggleFullscreen(){
     }
 }
 
-var canvas = getId("visCanvas").getContext("2d");
+var fps = 0;
+var currFPS = 0;
+var lastSecond = 0;
+var fpsEnabled = 0;
+function toggleFPS(){
+    fpsEnabled = Math.abs(fpsEnabled - 1);
+}
+
+var canvasElement = getId("visCanvas");
+var canvas = canvasElement.getContext("2d");
 
 function globalFrame(){
     requestAnimationFrame(globalFrame);
-    if(winsize !== [window.innerWidth, window.innerHeight]){
+    if(winsize[0] !== window.innerWidth || winsize[1] !== window.innerHeight){
         winsize = [window.innerWidth, window.innerHeight];
         if(fullscreen){
             size = [window.innerWidth, window.innerHeight];
@@ -221,9 +356,22 @@ function globalFrame(){
         getId("visCanvas").height = size[1];
     }
     if(currVis !== "none"){
-        canvas.clearRect(0, 0, size[0], size[1]);
         analyser.getByteFrequencyData(visData);
         vis[currVis].frame();
+        if(fpsEnabled){
+            fps++;
+            var currSecond = (new Date().getSeconds());
+            if(currSecond !== lastSecond){
+                currFPS = fps;
+                fps = 0;
+                lastSecond = currSecond;
+            }
+            canvas.font = '12px aosProFont, monospace';
+            canvas.fillStyle = 'rgba(0, 0, 0, 0.5)';
+            canvas.fillRect(1, 1, 14, 9);
+            canvas.fillStyle = '#FFF';
+            canvas.fillText(String(currFPS), 2.5, 9);
+        }
     }
 }
 
@@ -243,10 +391,36 @@ var colors = {
             return 'rgb(0,' + amount + ',' + (255 - amount) + ')';
         }
     },
+    fire: {
+        name: "Fire",
+        func: function(amount){
+            return 'rgb(' +
+                amount + ',' +
+                ((amount > 200) * amount * 0.25 + (amount > 127) * amount * 0.5 + (amount <= 127) * amount * 0.1) + ',0)';
+        }
+    },
+    white: {
+        name: "White",
+        func: function(amount){
+            return '#FFF';
+        }
+    },
+    red: {
+        name: "Red",
+        func: function(amount){
+            return '#A00';
+        }
+    },
     redglow: {
         name: "Red Glow",
         func: function(amount){
             return 'rgb(' + amount + ',0,0)';
+        }
+    },
+    green: {
+        name: "Green",
+        func: function(amount){
+            return '#0A0';
         }
     },
     greenglow: {
@@ -255,18 +429,22 @@ var colors = {
             return 'rgb(0,' + amount + ',0)';
         }
     },
+    blue: {
+        name: "Blue",
+        func: function(amount){
+            return '#00A';
+        }
+    },
     blueglow: {
         name: "Blue Glow",
         func: function(amount){
             return 'rgb(0,0,' + amount + ')';
         }
     },
-    fire: {
-        name: "Fire",
+    indigo: {
+        name: "Indigo",
         func: function(amount){
-            return 'rgb(' +
-                amount + ',' +
-                ((amount > 200) * amount * 0.25 + (amount > 127) * amount * 0.5 + (amount <= 127) * amount * 0.1) + ',0)';
+            return 'indigo';
         }
     }
 }
@@ -316,6 +494,7 @@ var vis = {
             
         },
         frame: function(){
+            canvas.clearRect(0, 0, size[0], size[1]);
             var step = size[0] / 1024;
             var last = -1;
             var heightFactor = size[1] / 255;
@@ -375,24 +554,104 @@ var vis = {
             
         }
     },
-    spikes2: {
-        name: "Spikes 2",
+    monstercat: {
+        name: "Monstercat",
         start: function(){
             
         },
         frame: function(){
-            var step = size[0] / 4096;
+            canvas.clearRect(0, 0, size[0], size[1]);
+            var left = size[0] * 0.1;
+            var maxWidth = size[0] * 0.8;
+            var barWidth = maxWidth / 96;
+            var barSpacing = maxWidth / 64;
+            var maxHeight = size[1] * 0.5 - size[1] * 0.2;
+            
+            for(var i = 0; i < 64; i++){
+                var strength = 0;
+                for(var j = 0; j < 16; j++){
+                    //strength = Math.max(visData[i * 16 + j], strength);
+                    strength += visData[i * 16 + j];
+                }
+                strength /= 16;
+                
+                canvas.fillStyle = getColor(strength);
+                canvas.fillRect(
+                    Math.round(left + i * barSpacing),
+                    Math.floor(size[1] / 2) - Math.round(strength / 255 * maxHeight),
+                    Math.round(barWidth),
+                    Math.round(strength / 255 * maxHeight + 5)
+                );
+            }
+            canvas.fillStyle = '#FFF';
+            canvas.font = (size[1] * 0.25) + 'px aosProFont, sans-serif';
+            canvas.fillText(fileNames[currentSong][0].toUpperCase(), Math.round(left), size[1] * 0.75, Math.floor(maxWidth));
+        },
+        stop: function(){
+            
+        }
+    },
+    rings: {
+        name: "Rings",
+        start: function(){
+            this.ringPositions = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        },
+        frame: function(){
+            canvas.clearRect(0, 0, size[0], size[1]);
+            var ringHeight = Math.round(Math.min(size[0], size[1]) * 0.8);
+            var ringWidth = Math.round(ringHeight * 0.023);
+            canvas.lineWidth = ringWidth;
+            canvas.lineCap = "round";
+            var ringPools = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            //var ringAvgs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            var center = [Math.round(size[0] / 2), Math.round(size[1] / 2)];
+            for(var i = 0; i < 1024; i++){
+                var currPool = Math.floor(i / 102.4);
+                ringPools[currPool] = Math.max(visData[i], ringPools[currPool]);
+                //ringPools[currPool] += visData[i];
+                //ringAvgs[currPool]++;
+            }
+            //for(var i in ringPools){
+            //    ringPools[i] /= ringAvgs[i];
+            //}
+            for(var i = 0; i < 10; i++){
+                var strength = Math.pow(ringPools[i], 2) / 65025;
+                this.ringPositions[i] += strength * 5;
+                if(this.ringPositions[i] >= 360){
+                    this.ringPositions[i] -= 360;
+                }
+                canvas.strokeStyle = getColor(strength * 255);
+                this.degArc(center[0], center[1], ringWidth * 2 * (i + 1), this.ringPositions[i], this.ringPositions[i] + 180);
+            }
+        },
+        stop: function(){
+            canvas.lineWidth = 1;
+            canvas.lineCap = "butt";
+        },
+        ringPositions: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        TAU: Math.PI * 2,
+        degArc: function(x, y, r, a, b){
+            canvas.beginPath();
+            canvas.arc(x, y, r, (a / 360) * this.TAU, (b / 360) * this.TAU);
+            canvas.stroke();
+        }
+    },
+    spectrogram: {
+        name: "Spectrogram",
+        start: function(){
+            canvas.clearRect(0, 0, size[0], size[1]);
+        },
+        frame: function(){
+            canvas.putImageData(canvas.getImageData(0, 0, size[0], size[1]), 0, -1);
+            var step = size[0] / 1024;
             var last = -1;
             var heightFactor = size[1] / 255;
-            for(var i = 0; i < 4096; i++){
+            for(var i = 0; i < 1024; i++){
                 var strength = 0;
                 if(i === 0){
                     strength = visData[i];
-                    canvas.strokeStyle = getColor(strength);
-                    canvas.beginPath();
-                    canvas.moveTo(0.5, size[1]);
-                    canvas.lineTo(0.5, (255 - strength) * heightFactor);
-                    canvas.stroke();
+                    canvas.fillStyle = getColor(strength);
+                    canvas.fillRect(0, size[1] - 1, 1, 1);
                 }else{
                     var last = Math.floor(step * (i - 1));
                     var curr = Math.floor(step * i);
@@ -402,36 +661,24 @@ var vis = {
                         for(var j = 0; j < curr - last - 1; j++){
                             //strength = ((j + 1) / (curr - last + 1) * visData[i - 1] + (curr - last - j + 1) / (curr - last + 1) * visData[i]);
                             strength = (visData[i] + visData[i - 1]) / 2;
-                            canvas.strokeStyle = getColor(strength);
-                            canvas.beginPath();
-                            canvas.moveTo(curr - j - 0.5, size[1]);
-                            canvas.lineTo(curr - j - 0.5, (255 - strength) * heightFactor);
-                            canvas.stroke();
+                            canvas.fillStyle = getColor(strength);
+                            canvas.fillRect(curr - j - 1, size[1] - 1, 1, 1);
                         }
                         strength = visData[i];
-                        canvas.strokeStyle = getColor(strength);
-                        canvas.beginPath();
-                        canvas.moveTo(curr + 0.5, size[1]);
-                        canvas.lineTo(curr + 0.5, (255 - strength) * heightFactor);
-                        canvas.stroke();
+                        canvas.fillStyle = getColor(strength);
+                        canvas.fillRect(curr, size[1] - 1, 1, 1);
                     }else if(curr === last && next > curr){
                         // compressed
                         for(var j = 0; j < (1 / step); j++){
                             strength += visData[i - j];
                         }
                         strength /= Math.floor(1 / step) + 1;
-                        canvas.strokeStyle = getColor(strength);
-                        canvas.beginPath();
-                        canvas.moveTo(curr + 0.5, size[1]);
-                        canvas.lineTo(curr + 0.5, (255 - strength) * heightFactor);
-                        canvas.stroke();
+                        canvas.fillStyle = getColor(strength);
+                        canvas.fillRect(curr, size[1] - 1, 1, 1);
                     }else if(last === curr - 1){
                         strength = visData[i];
-                        canvas.strokeStyle = getColor(strength);
-                        canvas.beginPath();
-                        canvas.moveTo(curr + 0.5, size[1]);
-                        canvas.lineTo(curr + 0.5, (255 - strength) * heightFactor);
-                        canvas.stroke();
+                        canvas.fillStyle = getColor(strength);
+                        canvas.fillRect(curr, size[1] - 1, 1, 1);
                     }
                 }
             }
@@ -446,6 +693,7 @@ var vis = {
             
         },
         frame: function(){
+            canvas.clearRect(0, 0, size[0], size[1]);
             var step = size[0] / 1024;
             var last = -1;
             for(var i = 0; i < 1024; i++){
@@ -504,6 +752,30 @@ var vis = {
             
         }
     },
+    solidColor: {
+        name: "Solid Color",
+        start: function(){
+            
+        },
+        frame: function(){
+            var avg = 0;
+            var avgtotal = 0;
+            for(var i = 0; i < 1024; i++){
+                if(visData[i] > 75){
+                    avg += visData[i];
+                    avgtotal++;
+                }
+            }
+            avg /= (avgtotal || 1);
+            //avg /= 1024;
+            //avg *= 255;
+            canvas.fillStyle = getColor(avg);
+            canvas.fillRect(0, 0, size[0], size[1]);
+        },
+        stop: function(){
+            
+        }
+    }
 };
 
 for(var i in colors){
