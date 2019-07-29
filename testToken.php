29d48769d3bf51ba9e0256aa05fd967282ee1d91
MineAndCraft12 <mineandcraft12@gmail.com>
2019-07-29 13:46:52 -0700

    debug commit 2

--- testToken.php
+++ testToken.php
@@ -3,8 +3,14 @@
         if(file_exists('logins/'.$_COOKIE['keyword'].'.txt')){
             if(password_verify($_COOKIE['logintoken'], file_get_contents('logins/'.$_COOKIE['keyword'].'.txt'))){
                 echo "1";
+            }else{
+                echo "failed verify<br>";
             }
+        }else{
+            echo "failed exist<br>";
         }
+    }else{
+        echo "failed is_dir<br>";
     }
     echo "0";
 ?>
