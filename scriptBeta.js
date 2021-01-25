62f3a7451eb73b011d8da60ee8360ae41e68cc2b
Aaron <mineandcraft12@gmail.com>
2021-01-25 14:19:34 -0700

    Increment version number

--- scriptBeta.js
+++ scriptBeta.js
@@ -11304,7 +11304,7 @@ c(function(){
         },
         oldVersions: "aOS has undergone many stages of development. Older versions are available at https://aaronos.dev/AaronOS_Old/"
     }; // changelog: (using this comment to make changelog easier for me to find)
-    window.aOSversion = 'B1.5.1.0 (01/24/2020) r1';
+    window.aOSversion = 'B1.5.2.0 (01/24/2020) r0';
     document.title = 'AaronOS ' + aOSversion;
     getId('aOSloadingInfo').innerHTML = 'Properties Viewer';
 });
